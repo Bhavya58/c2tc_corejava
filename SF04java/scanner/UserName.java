9c95bd4a81c3ed5c21f8d5d8e6c9a7425b5ac876
BHAVYASHREE K S <BHAVYASHREE K S@DESKTOP-UULHJB2>
2024-06-11 19:43:30 +0530

    third commit

---
+++ SF04java/scanner/UserName.java
@@ -0,0 +1,18 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class UserName {
+	private static Scanner input;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the user name:");
+		
+		String n =input.next();
+		
+		System.out.println("User Name:"+n);
+		
+	}
+
+}
