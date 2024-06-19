62f2dc030b6dcb8b6353b6be4261a1efdbbc3c86
BHAVYASHREE K S <BHAVYASHREE K S@DESKTOP-UULHJB2>
2024-06-19 15:23:33 +0530

    first commit

---
+++ SF04java/edu/svit/sfo4java/abstraction/Airtel.java
@@ -0,0 +1,19 @@
+package edu.svit.sfo4java.abstraction;
+
+public class Airtel implements Sim {
+
+	@Override
+	public void makeCall() {
+		// TODO Auto-generated method stub
+		System.out.println("make call using Airtel sim");
+	}
+
+	@Override
+	public void sendMsg() {
+		// TODO Auto-generated method stub
+		System.out.println("send msg using Airtel sim");
+	}
+	
+	
+
+}
---
+++ SF04java/edu/svit/sfo4java/abstraction/Sim.java
@@ -0,0 +1,11 @@
+package edu.svit.sfo4java.abstraction;
+
+public interface Sim {
+	
+	public void makeCall();
+	public void sendMsg();
+	
+	public static final float MIN_BALANCE=299;
+	int x=10;
+
+}
